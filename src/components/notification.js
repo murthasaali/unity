7df26ae0679be445abb37f5859216000bd4677c0
murthasaali <murthasaalick123@gmail.com>
2024-04-06 02:52:44 +0530

    updated response.data

--- src/components/notification.js
+++ src/components/notification.js
@@ -11,7 +11,7 @@ const fetchNotifications = async () => {
           Authorization: `Bearer ${token}`
         }
       });
-      console.log(response,data)
+      console.log(response.data)
           return response.data
   } catch (error) {
     throw new Error('Failed to fetch notifications');
